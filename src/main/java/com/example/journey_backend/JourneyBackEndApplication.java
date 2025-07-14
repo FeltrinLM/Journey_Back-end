66cc27956e6e230b79ee45069592ec649b75e4bb
FeltrinLM <lorenzofmendes49@gmail.com>
2025-07-13 22:52:57 -0300

    Initial commit

---
+++ src/main/java/com/example/journey_backend/JourneyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.journey_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneyBackEndApplication.class, args);
+    }
+
+}
